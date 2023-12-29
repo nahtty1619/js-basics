6a6f7fa5378fab65f152a07cd131cff9af659c43
nahtty1619 <natnaelgaromsa741125@gmail.com>
2023-12-29 19:16:51 +0300

    build the basics of  javascript

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 console.log('Hello world'); 
+//variable //
+let name='natnael';
+console.log(name);
+
+
+let firstName = 'natnael';
+let lastName = 'garomsa';
+
+console.log(firstName,lastName);
+
+//constant//
+const interestRate= 0.3;
+console.log(interestRate);
+
+
+//primitive type//
+
+let namee= 'nati';//string literales
+let age = 30; //number literales
+let isApproved = true; //boolean literales
+let firstNamee = undefined;//
+let selectedcolorr = null;
+
+console.log(isApproved);
+
+
+//reference type object
+
+let person={
+    name: 'nati',
+    age: 22
+};
+console.log(person);
+
+//Arrays//
+
+let selectedcolor = ['red', 'green']
+selectedcolor [2]= 'blue';
+console.log(selectedcolor);
+
+//function
+
+function greet(name, lastName) {
+    console.log('hello'+' ' + name + ' ' + lastName);
+};
+greet('nahtty', 'garomsa');
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+  
+
